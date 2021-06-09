ffb8288353292773a024831d2563f7d2e4fbaafb
barars <1286561513@qq.com>
2021-06-09 14:44:11 +0800

    update the code about queue!

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # DataStructProtices
+本仓库主要用作同步数据结构相关作业
--- queue&stack/queue/main.cpp
+++ queue&stack/queue/main.cpp
@@ -1,62 +1,98 @@
 #include<iostream>
+#include<assert.h>
 using namespace std;
+const int maxSize=50;
 
 template<class T>
-class queue{
-    public:
-        queue(int n){
-            dataP=new T[n];
-            maxN=n;
-            begin=0;
-            end=0;
-        }
-        ~queue(){
-            delete []dataP;
-        }
-        bool isEmpty(){
-           return (begin==end);
-        }
-        void push(T data){
-            dataP[++end]=data;
-        }
-        T pop(){
-            if(begin<end){
-                return dataP[++begin];
-                if(begin==end){
-                    begin=0,end=0;
-                }
-            }
-            return (T)NULL;
-        }
-        int size(){
-            return end-begin;
-        }
-    private:
-        T *dataP;
-        int maxN;
-        int begin;
-        int end;
+class Queue{
+public:
+    Queue(){};
+    ~Queue(){};
+    virtual bool EnQueue(const T& x)=0;
+    virtual bool DeQueue(T& x)=0;
+    virtual bool getFront(T& x)=0;
+    virtual bool IsEmpty()const=0;
+    virtual bool IsFull()const=0;
+    virtual int getSize()const=0;
 };
 
-int main(){
-    queue<char> q(100);
-    if(q.isEmpty()){
-        cout<<"YES"<<endl;
-    }
-    else{
-        cout<<"NO"<<endl;
-    }
-    q.push('a');
-    q.push('b');
-    q.push('c');
-    cout<<q.pop()<<endl;
-    q.push('d');
-    q.push('e');
-    q.push('f');
-    while(!(q.isEmpty())){
-        cout<<q.pop()<<" ";
-    }
-    cout<<endl;
-    q.~queue();
+template<class T>
+class SeqQueue:public Queue<T>{
+public:
+    SeqQueue(int sz=10);
+    ~SeqQueue(){delete[]elements;}
+    bool EnQueue(const T &x);
+    bool DeQueue(T &x);
+    bool getFront(T &x);
+    void makeEmpty(){front=rear=0;}
+    bool IsEmpty()const{return (front==rear)?true:false;}
+    bool IsFull()const{return ((rear+1)%maxSize==front)?true:false;}
+    int getSize()const{return (rear-front+maxSize)%maxSize;}
+    template<class T2>
+    friend ostream& operator<<(ostream& os,SeqQueue<T2>& Q);
+protected:
+    int front,rear;
+    T *elements;
+    int maxSize;
+};
+
+template<class T>
+SeqQueue<T>::SeqQueue(int sz):front(0),rear(0),maxSize(sz){
+    elements=new T[maxSize];
+    assert(elements!=NULL);
+}
+
+template<class T>
+bool SeqQueue<T>::EnQueue(const T& x){
+    if(IsFull()==true)
+        return false;
+    elements[rear]=x;
+    rear=(rear+1)%maxSize;
+    return true;
+}
+
+template<class T>
+bool SeqQueue<T>::DeQueue(T& x){
+    if(IsEmpty()==true)
+        return false;
+    x=elements[front];
+    front=(front+1)%maxSize;
+    return true;
+}
+
+template<class T>
+bool SeqQueue<T>::getFront(T& x){
+    if(IsEmpty()==true)
+        return false;
+    x=elements[front];
+    return true;
+}
+
+template<class T>
+ostream& operator<<(ostream& os,SeqQueue<T>& Q){
+    os<<"front="<<Q.front<<",rear="<<Q.rear<<endl;
+    for(int i=Q.front;i!=Q.rear;i=(i+1)%maxSize)
+        os<<i<<":"<<Q.elements[i]<<endl;
+    return os;
+}
+
+int main()
+{
+    char x;
+    SeqQueue<char> m;
+    if(m.IsEmpty())
+        cout<<"队列为空"<<endl;
+    else cout<<"队列不为空"<<endl;
+    for(int i=0;i<3;++i)
+        m.EnQueue(97+i);
+    if(m.DeQueue(x))
+        cout<<"出队元素为:"<<x<<endl;
+    else cout<<"出队失败!"<<endl;
+    cout<<"队列元素个数为:"<<m.getSize()<<endl;
+    for(int i=0;i<3;++i)
+        m.EnQueue(100+i);
+    cout<<m;
+    m.~SeqQueue();
     return 0;
 }
+
